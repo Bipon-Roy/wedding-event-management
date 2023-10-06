f7a47209b192052fc2ff734a4f521077b062d493
Bipon Roy <biponroy400@gmail.com>
2023-10-06 20:52:33 +0600

    first commit

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
