eb421e8a4ef45e462ef04babe5a9af8cc29d4982
Bipon Roy <biponroy400@gmail.com>
2023-10-06 22:16:35 +0600

    added navbar and footer

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+const Error = () => {
+    return (
+        <div className="">
+            <h1>
+                <span className="">404!</span> Nothings Found Here
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,63 @@
+import { NavLink } from "react-router-dom";
+import userInitial from "../../../assets/user.png";
+import logo from "../../../assets/logo.png";
 const Navbar = () => {
+    const links = (
+        <>
+            <li className="mr-5 font-semibold">
+                <NavLink to="/">Home</NavLink>
+            </li>
+            <li className="mr-5 font-semibold">
+                <NavLink to="/about">About</NavLink>
+            </li>
+            <li className="mr-5 font-semibold">
+                <NavLink to="/portfolio">Career</NavLink>
+            </li>
+            <li className="mr-5 font-semibold">
+                <NavLink to="/login">Sign In</NavLink>
+            </li>
+        </>
+    );
     return (
         <div className="max-w-7xl mx-auto">
-            <h1>Navbar</h1>
+            <div className="navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            {links}
+                        </ul>
+                    </div>
+                    <img src={logo} className="" alt="" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className=" menu-horizontal px-1">{links}</ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="w-10 rounded-full mr-5">
+                        <img src={userInitial} />
+                    </div>
+                    <a className="btn">Login</a>
+                </div>
+            </div>
         </div>
     );
 };
